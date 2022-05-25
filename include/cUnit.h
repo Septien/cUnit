40f92e99f3e27a32f6ad337d646f4858daaa4da3
Jose Septien <jaseptienh@gmail.com>
2022-05-25 13:49:12 -0500

    Add the data structures and framework interface.

--- include/cUnit.h
+++ include/cUnit.h
@@ -11,6 +11,54 @@
 #ifndef C_UNIT_H
 #define C_UNIT_H
 
-void init(void);
+#ifndef FUNCTION_NAME_SIZE
+#define FUNCTION_NAME_SIZE  40
+#endif
+
+typedef struct Test
+{
+    char function_name[FUNCTION_NAME_SIZE];
+    void *data_pointer;
+    Test *next;
+}Test_t;
+
+
+typedef struct cUnit
+{
+    void (*setup)(void);
+    void (*teardown)(void);
+    Test_t *head;
+}cUnit_t;
+
+/**
+ * @brief Initialization of the cUnit_t data structure. It receives the setup,
+ * teardown function, and the data for the set of tests, and allocates the
+ * resources needed.
+ * 
+ * @param setup 
+ * @param teardown 
+ * @param data 
+ */
+void init(void (*setup)(void), void (*teardown)(void), void *data);
+
+/**
+ * @brief Releases all the resourcess used.
+ * 
+ */
+void terminate(void);
+
+/**
+ * @brief Add a test to the list.
+ * 
+ * @param function_name 
+ * @param test 
+ */
+void add_test(char *function_name, void (*test)(void));
+
+/**
+ * @brief Execute all the allocated tests.
+ * 
+ */
+void execute_test(void);
 
 #endif      // C_UNIT_H
