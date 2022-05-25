b76a18da7678f61adca157435fcdb5d89668ca99
Jose Septien <jaseptienh@gmail.com>
2022-05-25 16:07:37 -0500

    Add the cunit_function.

--- cUnit.c
+++ cUnit.c
@@ -1,6 +1,26 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "assert.h"
+
 #include "cUnit.h"
 
-void init(void)
+#ifdef __RIOT__
+#include "malloc.h"
+#endif
+
+void cunit_init(cUnit_t **cUnit, void (*setup)(void), void (*teardown)(void), void *data)
 {
-    return;
+    assert(cUnit != NULL);
+    assert(setup != NULL);
+    assert(teardown != NULL);
+    assert(data != NULL);
+
+    // Allocate memory
+    *cUnit = (cUnit_t *)malloc(sizeof(cUnit_t));
+
+    // Store the setup and teardown functions.
+    (*cUnit)->setup = setup;
+    (*cUnit)->teardown = teardown;
+    (*cUnit)->data = data;
+    (*cUnit)->head = NULL;
 }
--- tests/cunit_tests.c
+++ tests/cunit_tests.c
@@ -1,11 +1,61 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
+#include "assert.h"
 #include "cUnit.h"
 #include "cunit_tests.h"
+
+#ifdef __RIOT__
+#include "malloc.h"
+#endif
+
+void dummy_setup(void)
+{
+    return;
+}
+
+void dummy_teardown(void)
+{
+    return;
+}
+
+struct test_st
+{
+    int a;
+    int b;
+};
+
+
+void test_cunit_init(void)
+{
+    cUnit_t *cUnit = NULL;
+
+    struct test_st st, *st2;
+    st2 = &st;
+
+    cunit_init(&cUnit, &dummy_setup, &dummy_teardown, st2);
+    assert(cUnit != NULL);
+    assert(cUnit->setup == dummy_setup);
+    assert(cUnit->teardown == dummy_teardown);
+    assert(cUnit->data == st2);
+    assert(cUnit->head == NULL);
+
+    cUnit->setup = NULL;
+    cUnit->teardown = NULL;
+    cUnit->data = NULL;
+    free(cUnit);
+}
+
+
 
 void cunit_tests(void)
 {
     printf("Testing the cUnit framework.\n");
+
+    printf("Testing the cunit_init function.\n");
+    test_cunit_init();
+    printf("Test passed.\n");
+
     return;
 }
