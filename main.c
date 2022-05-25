082d8da7a3e4466cd1fa690555675f9daab711d5
Jose Septien <jaseptienh@gmail.com>
2022-05-25 16:07:13 -0500

    Format main function to comply with each OS.

--- main.c
+++ main.c
@@ -6,7 +6,18 @@
 
 int main(void)
 {
+#ifdef __LINUX__
+    printf("Testing the cUnit framework.\n");
     printf("cUnit framework.\n");
     cunit_tests();
+#endif
+#ifdef __RIOT__
+    puts("\nWelcome to RIOT!.\n");
+    do {
+        printf("Testing the cUnit framework.\n");
+        printf("cUnit framework.\n");
+        cunit_tests();
+    }while (0);
+#endif
     exit(0);
 }
